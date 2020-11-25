afc4287da575fc3bf84792a36dd1b6e06e10465d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-11-25 16:27:18 +0700

    Nuxt Watcher

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,65 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
-    <span :title="message"> Test Test </span>
+    <ol>
+      <TodoItem v-for="todo in todos" :key="todo.id" :item="todo"></TodoItem>
+    </ol>
+    <br />
+    <hr />
+    <br />
+    <p>
+      Ask a yes/no question:
+      <input v-model="question" />
+    </p>
+    <p>{{ answer }}</p>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem'
+import _ from 'lodash'
+import axios from 'axios'
+
 export default {
+  components: {
+    TodoItem,
+  },
   data() {
     return {
-      message: 'Hello Vue',
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!',
+      todos: [
+        { id: 1, text: 'Vue Js' },
+        { id: 2, text: 'Angular' },
+        { id: 3, text: 'React' },
+      ],
     }
+  },
+  watch: {
+    question(newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    },
+  },
+  created() {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer() {
+      if (!this.question.includes('?')) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      const vm = this
+      axios
+        .get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    },
   },
 }
 </script>
